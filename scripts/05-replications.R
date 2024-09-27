b6a499e8681b2975c7f08fa4609c57ee1284e3bf
qizhou xie <qizhou.xie@mail.utoronto.ca>
2024-09-27 15:18:17 -0400

    updata

---
+++ scripts/05-replications.R
@@ -0,0 +1,19 @@
+#### Preamble ####
+# Purpose: Replicated graphs from... [...UPDATE THIS...]
+# Author: qizhou xie [...UPDATE THIS...]
+# Date: 24 september 2024 [...UPDATE THIS...]
+# Contact: qizhou.xie@mail.utoronto.ca [...UPDATE THIS...]
+# License: MIT
+# Pre-requisites: [...UPDATE THIS...]
+# Any other information needed? [...UPDATE THIS...]
+
+
+#### Workspace setup ####
+library(tidyverse)
+# [...UPDATE THIS...]
+
+#### Load data ####
+# [...ADD CODE HERE...]
+
+
+
